3cfbd822410b0795ed7030d821288df352509769
Liam <155930200+lpoconnor@users.noreply.github.com>
2025-11-25 13:04:44 +0000

    Update SphericalCoordinates.lean

--- PhysLean/Mathematics/SphericalCoordinates/SphericalCoordinates.lean
+++ PhysLean/Mathematics/SphericalCoordinates/SphericalCoordinates.lean
@@ -14,9 +14,9 @@ import Mathlib
 This module implements the fundamentals of spherical coordinates
 
 # Standard Notation
-r  - radial distance from origin (>= 0)
-θ  - polar angle measured from the positive z axis (0 <= θ <= pi)
-φ  - azimuthal angle, measured from positive x axis (−pi < φ ≤ pi)
+r - radial distance from origin (>= 0)
+θ - polar angle measured from the positive z axis (0 <= θ <= pi)
+φ - azimuthal angle, measured from positive x axis (−pi < φ ≤ pi)
 
 Cartesian to spherical conversion:
 r = sqrt (x^2 + y^2 + z^2)
@@ -24,38 +24,38 @@ r = sqrt (x^2 + y^2 + z^2)
 φ = atan2(y, x)
 
 Spherical to cartesian conversion:
-x = r sin θ cos φ
-y = r sin θ sin φ
-z = r cos θ
+x = r sinθ cosφ
+y = r sinθ sinφ
+z = r cosθ
 
 Distance between two spherical coordinates can be done by converting to Cartesian and doing so
 -/
 
 structure Spherical where
-  r     : Real
+  r : Real
   theta : Real
-  phi   : Real
+  phi : Real
 deriving Repr, BEq
 
 namespace Spherical
 
 def toCartesianVec3 (s : Spherical) : Vec3 :=
-  let x := s.r * Real.sin s.theta * Real.cos s.phi
-  let y := s.r * Real.sin s.theta * Real.sin s.phi
-  let z := s.r * Real.cos s.theta
+  let x := s.r * Real.sin(s.theta) * Real.cos(s.phi)
+  let y := s.r * Real.sin(s.theta) * Real.sin(s.phi)
+  let z := s.r * Real.cos(s.theta)
   ⟨x, y, z⟩
 
 def Vec3.toSpherical (v : Vec3) : Spherical :=
   let x := v.x
   let y := v.y
   let z := v.z
-  let r := Real.sqrt (x*x + y*y + z*z)
+  let r := Real.sqrt(x*x + y*y + z*z)
 
   if h : r = 0 then
     { r := 0, theta := 0, phi := 0 }
   else
     let theta := Real.arccos (z / r)
-    let phi   := Real.atan2 y x
+    let phi := Real.atan2 y x
     { r := r, theta := theta, phi := phi }
 
 def dist (s1 s2 : Spherical) : Real :=
