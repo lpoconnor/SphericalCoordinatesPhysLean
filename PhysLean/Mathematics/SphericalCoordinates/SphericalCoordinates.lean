b89027c7de668fd181f3f7641b0253273f42e044
Liam <155930200+lpoconnor@users.noreply.github.com>
2025-12-12 00:24:27 +0000

    Update SphericalCoordinates.lean

--- PhysLean/Mathematics/SphericalCoordinates/SphericalCoordinates.lean
+++ PhysLean/Mathematics/SphericalCoordinates/SphericalCoordinates.lean
@@ -7,60 +7,42 @@ Author: Liam O'Connor
 import PhysLean.Meta.Informal.Basic
 import PhysLean.Meta.TODO.Basic
 import Mathlib
-
-/-
-# Spherical Coordinates
-
-This module implements the fundamentals of spherical coordinates
-
-# Standard Notation
-r - radial distance from origin (>= 0)
-θ - polar angle measured from the positive z axis (0 <= θ <= pi)
-φ - azimuthal angle, measured from positive x axis (−pi < φ ≤ pi)
-
-Cartesian to spherical conversion:
-r = sqrt (x^2 + y^2 + z^2)
-θ = arccos(z / r)
-φ = atan2(y, x)
-
-Spherical to cartesian conversion:
-x = r sinθ cosφ
-y = r sinθ sinφ
-z = r cosθ
-
-Distance between two spherical coordinates can be done by converting to Cartesian and doing so
--/
+import PhysLean.SpaceAndTime.Space.Basic
 
 structure Spherical where
-  r : Real
+  r     : Real
   theta : Real
-  phi : Real
-deriving Repr, BEq
+  phi   : Real
+deriving Repr
 
 namespace Spherical
 
-def toCartesianVec3 (s : Spherical) : Vec3 :=
-  let x := s.r * Real.sin(s.theta) * Real.cos(s.phi)
-  let y := s.r * Real.sin(s.theta) * Real.sin(s.phi)
-  let z := s.r * Real.cos(s.theta)
-  ⟨x, y, z⟩
+open EuclideanSpace
 
-def Vec3.toSpherical (v : Vec3) : Spherical :=
-  let x := v.x
-  let y := v.y
-  let z := v.z
-  let r := Real.sqrt(x*x + y*y + z*z)
+noncomputable def toCartesianSpace3 (s : Spherical) : Space 3 :=
+  EuclideanSpace.vec #[ s.r * Real.sin(s.theta) * Real.cos(s.phi),
+     s.r * Real.si(s.theta) * Real.sin(s.phi),
+     s.r * Real.cos(s.theta) ]
 
-  if h : r = 0 then
+noncomputable def Space.toSpherical (v : Space 3) : Spherical :=
+  let x := v 0
+  let y := v 1
+  let z := v 2
+  let r := Real.sqrt (x*x + y*y + z*z)
+  if _ : r = 0 then
     { r := 0, theta := 0, phi := 0 }
   else
-    let theta := Real.arccos (z / r)
-    let phi := Real.atan2 y x
+    let theta := Real.acos(z / r)
+    let phi :=
+      if x > 0 then Real.atan(y / x)
+      else if x < 0 ∧ y ≥ 0 then Real.atan(y / x) + Real.pi
+      else if x < 0 ∧ y < 0 then Real.atan(y / x) - Real.pi
+      else if x = 0 ∧ y > 0 then Real.pi / 2
+      else if x = 0 ∧ y < 0 then -Real.pi / 2
+      else 0
     { r := r, theta := theta, phi := phi }
 
-def dist (s1 s2 : Spherical) : Real :=
-  let c1 := s1.toVec3
-  let c2 := s2.toVec3
-  Vec3.dist c1 c2
+noncomputable def dist (s1 s2 : Spherical) : Real :=
+  EuclideanSpace.dist (s1.toCartesianSpace3) (s2.toCartesianSpace3)
 
 end Spherical
